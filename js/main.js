a8d32b324075eb8e0b828346ac9d4688883abeb5
Maaz Ahmed <elfilosof@protonmail.com>
2020-03-21 23:59:31 +0530

    FAQ first one

--- js/main.js
+++ js/main.js
@@ -28,3 +28,18 @@ floating_btn.addEventListener("click", () => {
 close_btn.addEventListener("click", () => {
   social_panel_container.classList.remove("visible");
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
